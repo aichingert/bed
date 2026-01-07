338629a98a2e80198159670025db39b43ec2a14d
Tobias Aichinger <aichinger.tobias13@gmail.com>
2026-01-07 18:19:20 +0100

    sync with lap

--- src/bed.c
+++ src/bed.c
@@ -4,6 +4,23 @@ s32 main(s32 argc, char **argv, u8 **environ) {
     init_ctx(environ);
     init_tui();
 
+    String color = TUI_SET_COLOR(255, 255, 0);
+    //printf("%s\n", color.val);
+    os_write(STD_IO_FD, color.val, color.len); 
+
+    String hex_value = S("0xbaa112 ");
+    String mnemonic  = S("mov ");
+    String registerv = S("rax");
+    String comma     = S(", ");
+    String literal   = S("60\n");
+
+    os_write(STD_IO_FD, hex_value.val, hex_value.len);
+    os_write(STD_IO_FD, mnemonic.val, mnemonic.len);
+    os_write(STD_IO_FD, registerv.val, registerv.len);
+    os_write(STD_IO_FD, comma.val, comma.len);
+    os_write(STD_IO_FD, literal.val, literal.len);
+    os_write(STD_IO_FD, TUI_RESET_COLOR.val, TUI_RESET_COLOR.len); 
+
     u8 buf[4096];
     memset(buf, 0, mob_static_array_len(buf));
     os_read(STD_IO_FD, buf, mob_static_array_len(buf));
--- src/tui.c
+++ src/tui.c
@@ -1,12 +1,26 @@
-#define STD_IO_FD   1
+#define STD_IO_FD               1
 
-#define TUI_OPEN_BUF    S("\e[?1049h")
-#define TUI_CLOSE_BUF   S("\e[?1049l")
-#define TUI_CLEAR       S("\e[2j")
+#define TUI_SET_COLOR(r,g,b)    S("\e[38;2;" #r ";" #g ";" #b "m")
+#define TUI_SET_POSITION(x, y)  S("\e[" #y ";" #x "H")
+
+const String TUI_OPEN_BUF       = S("\e[?1049h");
+const String TUI_CLOSE_BUF      = S("\e[?1049l");
+const String TUI_CLEAR          = S("\e[2j");
+const String TUI_RESET_COLOR    = S("\e[0m");
+const String TUI_LEFT_TOP       = TUI_SET_POSITION(1, 1);
+
+const String TUI_COLOR_NAVY     = TUI_SET_COLOR(45, 60, 89);
+const String TUI_COLOR_SAGE     = TUI_SET_COLOR(148, 163, 120);
+const String TUI_COLOR_YELLOW   = TUI_SET_COLOR(229, 186, 65);
+const String TUI_COLOR_ORANGE   = TUI_SET_COLOR(209, 133, 92);
+
+const String TUI_FG             = S("\e[48;2;197;200;198m");
+const String TUI_BG             = S("\e[48;2;29;31;33m");
 
 void init_tui() {
     os_write(STD_IO_FD, TUI_OPEN_BUF.val, TUI_OPEN_BUF.len);
     os_write(STD_IO_FD, TUI_CLEAR.val, TUI_CLEAR.len);
+    os_write(STD_IO_FD, TUI_LEFT_TOP.val, TUI_LEFT_TOP.len);
 }
 
 void exit_tui() {
